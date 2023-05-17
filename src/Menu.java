577c74ba19e5aa9bceb29d582088c3b146e54c66
Leonardo Belizario <leonardobelizario88@gmail.com>
2023-05-17 16:32:59 -0300

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -14,13 +14,13 @@ public class Menu {
             String mensagem = sc.nextLine();
             System.out.println();
 
-            // Criptografar a mensagem
+            
 
             System.out
                     .println("\u001B[47m\u001B[30mMensagem criptografada:\u001B[0m "
                             + Criptografar.criptografar(mensagem));
             System.out.println();
-            // Descriptografar a mensagem
+           
             System.out.println("\u001B[45mVoce deseja descriptografar a mensagem? (S/N)\u001B[0m");
 
             String resposta = sc.nextLine();
